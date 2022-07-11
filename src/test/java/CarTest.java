4a1e6b4143778bfad1c2e50b0239d979fb9de946
Saeid Tadrisi <saeid.tadrisi@gmail.com>
2022-07-11 21:13:31 +0200

    Machine Test Created

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,40 +1,95 @@
-import static java.lang.System.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class CarTest {
 
+    @Test
+    void carTest() {
+        Car car = new Car();
+        car.doorsAreOpen();
+        car.carIsTurnedOn();
+        car.carIsSafe();
+        car.carIsMoving();
+        System.out.println(car.carIsMoving());
+
+        Car bmw = new Car("BMW");
+        bmw.doorAreClose();
+        bmw.carIsTurnedOn();
+        bmw.carIsSafe();
+        bmw.carIsMoving();
+    }
+
+    @Test
+    void moving() {
+        Car benz = new Car("Benz");
+
+        benz.carIsTurnedOn();
+        benz.carIsSafe();
+        boolean benzMoving = benz.carIsMoving();
+        Assertions.assertTrue(benzMoving);
+
+        Car fiat = new Car("Fiat");
+        fiat.carIsSafe();
+        boolean volvoMoving = fiat.carIsMoving();
+        Assertions.assertFalse(fiat.carIsMoving());
+    }
 
 
-    class car {
+    class Car {
 
-        boolean doorsAreOpen;
-        boolean carIsStarted;
+        boolean doors;
+        boolean start;
+        boolean safety;
+        String brand;
 
-        void doorsAreOpen (){
-            out.println("Doors Are Open");
-            doorsAreOpen = true;
+        Car (){
+            System.out.println("New Car Created");
         }
-        void doorsIsClose(){
-            out.println("Doors Are Close");
-            doorsAreOpen = false;
+
+        Car (String brand){
+            this.brand = brand;
+            System.out.println("New Car With "+ brand +" Brand Created");
         }
-        void carIsStarted (){
-            out.println("Car Is Started");
-            carIsStarted = true;
+
+        void doorsAreOpen(){
+            System.out.println("Doors Of The Car Are Open");
+            doors = true;
         }
+
+        void doorAreClose(){
+            System.out.println("Doors Of The Car Are Close");
+            doors = false;
+        }
+
+        void carIsTurnedOn(){
+            System.out.println("Car Is Turned On");
+            start = true;
+        }
+
         void carIsTurnedOff(){
-            out.println("Car Is Turned Off");
-            carIsStarted = false;
+            System.out.println("Car Is Turned Off");
+            start = false;
         }
 
-        void carIsMoving(){
-            if (!doorsAreOpen && carIsStarted){
-                out.println ("Car Is Moving");
+        void carIsSafe(){
+            System.out.println("Car Is Safe");
+            safety = true;
+        }
+
+        void carsIsNotSafe(){
+            System.out.println("Car Is Not Safe");
+            safety = false;
+        }
+
+        boolean carIsMoving(){
+            if (start && !doors && safety){
+                System.out.println("Car Is Moving");
+                return true;
             }else {
-                out.println("Car Is Parked");
+                System.out.println("Car Is Parked");
+                return false;
             }
         }
-
-
-
     }
+
 }
